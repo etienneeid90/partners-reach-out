9d51a7808877d89ea339fff915024fc1c0ed280a
Etienne Eid <etienneeid90@gmail.com>
2021-01-16 20:33:12 +0200

    implemented addRequestId in the app

--- reach-out-api-service/index.js
+++ reach-out-api-service/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const addRequestId = require('./public/middlewares/middleware')
 
 const app = express();
 
 // for parsing application/json
 app.use(express.json());
+app.use(addRequestId);
 
 app.listen(1330, () => console.log('Listening on port 1330...'));
