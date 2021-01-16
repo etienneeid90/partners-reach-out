343171d3fa42bea492826fa61535c844da42d857
Etienne Eid <etienneeid90@gmail.com>
2021-01-16 20:37:08 +0200

    implemented the partners route in the app

--- reach-out-api-service/index.js
+++ reach-out-api-service/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const addRequestId = require('./public/middlewares/addRequestId')
+const addRequestId = require('./public/middlewares/addRequestId');
+const partnersRouter = require('./public/routes/partners');
 
 const app = express();
 
 // for parsing application/json
 app.use(express.json());
 app.use(addRequestId);
+app.use('/partners', partnersRouter);
 
 app.listen(1330, () => console.log('Listening on port 1330...'));
