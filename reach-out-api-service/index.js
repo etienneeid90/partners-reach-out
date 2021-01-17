7e3622d41219fac17509199e1b15bc926ee41a15
Etienne Eid <etienneeid90@gmail.com>
2021-01-18 00:16:55 +0200

    added CORS

--- reach-out-api-service/index.js
+++ reach-out-api-service/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
 require('./public/startup/middlewares')(app);
 require('./public/startup/routes')(app);
 
